55c5a83205768ef339841bf83e7d36e041fbce02
Naty-S <na7a5cha010@gmail.com>
2021-07-09 13:18:42 -0400

    tests

--- 03-counter-app/src/setupTests.js
+++ 03-counter-app/src/setupTests.js
@@ -4,8 +4,8 @@
 // learn more: https://github.com/testing-library/jest-dom
 // import '@testing-library/jest-dom';
 // import '@testing-library/jest-dom/extend-expect';
-import Enzyme from 'enzyme';
 import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
+import Enzyme from 'enzyme';
 import { createSerializer } from 'enzyme-to-json'
 
 Enzyme.configure({ adapter: new Adapter() });
