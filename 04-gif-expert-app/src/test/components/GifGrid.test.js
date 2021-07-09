02e594f954350e15c1c7431c5f8f50c5d45cc416
Naty-S <na7a5cha010@gmail.com>
2021-07-09 17:34:37 -0400

    tests

--- 04-gif-expert-app/src/test/components/GifGrid.test.js
+++ 04-gif-expert-app/src/test/components/GifGrid.test.js
@@ -42,7 +42,7 @@ describe('Pruebas en el <GifGrid />', () => {
         const wrapper = shallow( <GifGrid category={ category } /> );
         
         // expect( wrapper ).toMatchSnapshot();
-        expect( wrapper.find('p').exists() ).toBe(false);
+        expect( wrapper.find('p').exists() ).toBe(false);  // el parrafo de loading ya no debe estar
         expect( wrapper.find('GifGridItem').length ).toBe( gifs.length );
     })
 })
